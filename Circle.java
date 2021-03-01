3f72dc0d46590da91e2f2db84dde3d122d090eef
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-01 15:23:12 -0600

    Added constructor and paintComponent

--- Circle.java
+++ Circle.java
@@ -1,3 +1,14 @@
-public class Circle {
-    
+import java.awt.*;
+
+public class Circle extends Shape {
+    public Circle(){
+        super();
+    }
+
+    public void paintComponent(Graphics g){
+        g.setColor(getSColor());
+        g.fillOval(getX(), getY(), getWidth(), getHeight());
+        g.setColor(getLColor());
+        g.drawString(getLetter(), getCenterX(), getCenterY());
+    }
 }

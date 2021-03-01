21cc99591ff6d4db85b15538fdcb15dfd809ff46
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-01 14:50:00 -0600

    Getters and setters added

--- Shape.java
+++ Shape.java
@@ -19,4 +19,23 @@ abstract class Shape {
     public final Color getSColor(){ return shapeColor; }
 
     public final Color getLColor(){ return letterColor; }
+
+    public final void setWidth(int w){ width = w; }
+
+    public final void setHeight(int h){ height = h; }
+
+    public final void setX(int x){ xPosition = x; }
+
+    public final void setY(int y){ yPosition = y; }
+
+    // Should only be a single character.
+    public final void setLetter(String l){ letter = l; }
+
+    public final void setSColor (Color s){shapeColor = s; }
+
+    public final void setSColor(int r, int g, int b){ shapeColor = new Color(r, g, b); }
+
+    public final void setLColor (Color l){letterColor = l; }
+
+    public final void setLColor(int r, int g, int b){ letterColor = new Color(r, g, b); }
 }

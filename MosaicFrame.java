4de8c872df149b20d940eb36d3960fa53cf37c56
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-02 16:23:40 -0600

    Added constructor

--- MosaicFrame.java
+++ MosaicFrame.java
@@ -1,3 +1,22 @@
-public class MosaicFrame {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MosaicFrame extends JFrame {
+    private MosaicPanel myMosaic;
+    private Container mosaicContainer;
+
+    MosaicFrame(){
+        setTitle("Mosaic");
+        setBounds(100, 100, 840, 840);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myMosaic = new MosaicPanel();
+
+        mosaicContainer = getContentPane();
+
+        mosaicContainer.add(myMosaic);
+
+
+    }
 }

c12f08d3782374773f347090945b9d65ad748264
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-01 23:08:33 -0600

    Added constructors and implemented paintShape

--- Circle.java
+++ Circle.java
@@ -5,10 +5,19 @@ public class Circle extends Shape {
         super();
     }
 
-    public void paintComponent(Graphics g){
-        g.setColor(getSColor());
-        g.fillOval(getX(), getY(), getWidth(), getHeight());
-        g.setColor(getLColor());
-        g.drawString(getLetter(), getCenterX(), getCenterY());
+    public Circle(int w, int h, int x, int y, String l, Color s, Color lC){
+        super(w, h, x, y, l, s, lC);
+    }
+
+    public Circle(int w, int h, int x, int y, String l, int sR, int sG, int sB, int lR, int lG, 
+                    int lB){
+        super(w, h, x, y, l, sR, sG, sB, lR, lG, lB);
+    }
+
+    public void paintShape(Graphics g){
+        g.setColor(shapeColor);
+        g.fillOval(xPosition, yPosition, width, height);
+        g.setColor(letterColor);
+        g.drawString(letter, centerX, centerY);
     }
 }
--- Square.java
+++ Square.java
@@ -1,3 +1,23 @@
-public class Square {
-    
+import java.awt.*;
+
+public class Square extends Shape {
+    Square(){
+        super();
+    }
+
+    public Square(int w, int h, int x, int y, String l, Color s, Color lC){
+        super(w, h, x, y, l, s, lC);
+    }
+
+    public Square(int w, int h, int x, int y, String l, int sR, int sG, int sB, int lR, int lG, 
+                    int lB){
+        super(w, h, x, y, l, sR, sG, sB, lR, lG, lB);
+    }
+
+    public void paintShape(Graphics g){
+        g.setColor(shapeColor);
+        g.fillRect(xPosition, yPosition, width, height);
+        g.setColor(letterColor);
+        g.drawString(letter, centerX, centerY);
+    }
 }

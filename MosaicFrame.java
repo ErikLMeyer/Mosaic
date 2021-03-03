91c9e7452eb86b3fcf7bbb2027169888293cfbc4
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-02 21:22:51 -0600

    Added a button. It doesn't do anything right now.

--- MosaicFrame.java
+++ MosaicFrame.java
@@ -4,18 +4,22 @@ import java.awt.event.*;
 
 public class MosaicFrame extends JFrame {
     private MosaicPanel myMosaic;
+    private JButton randomize;
     private Container mosaicContainer;
 
     MosaicFrame(){
         setTitle("Mosaic");
+        setLayout(new BorderLayout());
         setBounds(0, 0, 840, 840);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         myMosaic = new MosaicPanel();
+        randomize = new JButton("Randomize");
 
         mosaicContainer = getContentPane();
 
         mosaicContainer.add(myMosaic);
+        mosaicContainer.add(randomize, BorderLayout.SOUTH);
 
 
     }

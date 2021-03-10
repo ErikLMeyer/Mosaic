77d50f3c05cb44856353c04de3bcc9af4cb31138
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-09 18:53:49 -0600

    Clicked "Format Document"

--- MosaicPanel.java
+++ MosaicPanel.java
@@ -4,7 +4,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MosaicPanel extends JPanel implements MouseListener {
-    // Constants. Define number of objects to draw, and the difference in each of the rgb values.
+    // Constants. Define number of objects to draw, and the difference in each of
+    // the rgb values.
     private final int GRID_SIZE = 12;
     private final int COL_DIF = 50;
 
@@ -13,13 +14,13 @@ public class MosaicPanel extends JPanel implements MouseListener {
     private int height, width;
 
     // Fills tilling with a random assortment of Squares and Circles.
-    public void tile(){
-        for(int row = 0; row < GRID_SIZE; row++){
-            for (int col = 0; col < GRID_SIZE; col++){
-                int polygon = (int)(Math.random() * 2);
-                if (polygon == 0){
+    public void tile() {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                int polygon = (int) (Math.random() * 2);
+                if (polygon == 0) {
                     tilling[row][col] = new Square();
-                } else{
+                } else {
                     tilling[row][col] = new Circle();
                 }
             }
@@ -27,11 +28,11 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Determines size and position of Shape objects on screen
-    public void setDimensions(){
+    public void setDimensions() {
         width = super.getWidth() / GRID_SIZE;
         height = super.getHeight() / GRID_SIZE;
-        for(int row = 0; row < GRID_SIZE; row++){
-            for (int col = 0; col < GRID_SIZE; col++){
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
                 tilling[row][col].setWidth(width);
                 tilling[row][col].setHeight(height);
                 tilling[row][col].setX(row * width);
@@ -44,42 +45,42 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Returns a random capitalized letter.
-    public String generateLetters(){
-        char letterC = (char)(Math.floor((Math.random() * (0x5a - 0x41 + 1)) + 0x41));
+    public String generateLetters() {
+        char letterC = (char) (Math.floor((Math.random() * (0x5a - 0x41 + 1)) + 0x41));
         return Character.toString(letterC);
     }
 
     // Assigns each Shape a random letter.
-    public void setLetters(){
-        for (int row = 0; row < GRID_SIZE; row++){
-            for (int col = 0; col < GRID_SIZE; col++){
+    public void setLetters() {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
                 tilling[row][col].setLetter(generateLetters());
             }
         }
     }
 
     // Returns a Color with randomized rgb values.
-    public Color generateColor(){
-        int red = (int)(Math.random() * 256);
-        int green = (int)(Math.random() * 256);
-        int blue = (int)(Math.random() * 256);
+    public Color generateColor() {
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
 
         return new Color(red, green, blue);
     }
 
     // Returns an int value such that lCol is outside of a range of 50 from sCol.
-    public int adjustValue(int sCol, int lCol){
+    public int adjustValue(int sCol, int lCol) {
         int finalVal = lCol;
-        if (Math.abs(sCol - lCol) < COL_DIF){
-            if (lCol >= sCol){
-                if (lCol + COL_DIF >= 0xFF){
+        if (Math.abs(sCol - lCol) < COL_DIF) {
+            if (lCol >= sCol) {
+                if (lCol + COL_DIF >= 0xFF) {
                     finalVal = sCol - COL_DIF;
                 } else
                     finalVal = lCol + COL_DIF;
-            } else{
-                if (lCol - COL_DIF <= 0){
+            } else {
+                if (lCol - COL_DIF <= 0) {
                     finalVal = sCol + COL_DIF;
-                } else{
+                } else {
                     finalVal = lCol - COL_DIF;
                 }
             }
@@ -89,7 +90,7 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Returns a Color whose rgb values are noticeably different from sCol.
-    public Color adjustedColor(Color sCol, Color lCol){
+    public Color adjustedColor(Color sCol, Color lCol) {
         int red = adjustValue(sCol.getRed(), lCol.getRed());
         int green = adjustValue(sCol.getGreen(), lCol.getGreen());
         int blue = adjustValue(sCol.getBlue(), lCol.getBlue());
@@ -98,9 +99,9 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Assigns a randomized Color to each Shape and their letter.
-    public void setColors(){
-        for(int row = 0; row < GRID_SIZE; row++){
-            for (int col = 0; col < GRID_SIZE; col++){
+    public void setColors() {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
                 Color sCol = generateColor();
                 Color lCol = generateColor();
                 tilling[row][col].setSColor(sCol);
@@ -110,7 +111,7 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Creates a new MosaicPanel
-    public MosaicPanel(){
+    public MosaicPanel() {
         tilling = new Tile[GRID_SIZE][GRID_SIZE];
 
         tile();
@@ -122,7 +123,7 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Rerandomizes the Shapes in tilling.
-    public void retile(){
+    public void retile() {
         tile();
         setDimensions();
         setLetters();
@@ -130,24 +131,24 @@ public class MosaicPanel extends JPanel implements MouseListener {
     }
 
     // Paints Shape objects to screen.
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         setDimensions();
-        for (int row = 0; row < GRID_SIZE; row++){
-            for (int col = 0; col < GRID_SIZE; col++){
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
                 tilling[row][col].paintShape(g);
             }
         }
     }
 
-    public void mouseClicked(MouseEvent e){
-        int row = (int)((e.getX() / (double)super.getWidth()) * GRID_SIZE);
-        int col = (int)((double)(e.getY() / (double)super.getHeight()) * GRID_SIZE);
+    public void mouseClicked(MouseEvent e) {
+        int row = (int) ((e.getX() / (double) super.getWidth()) * GRID_SIZE);
+        int col = (int) ((double) (e.getY() / (double) super.getHeight()) * GRID_SIZE);
 
-        if (tilling[row][col].getDrawShape()){
+        if (tilling[row][col].getDrawShape()) {
             tilling[row][col].setDrawShape(false);
-        } else{
+        } else {
             tilling[row][col].setDrawShape(true);
         }
         repaint();
